66e18e43d6fe75967b527d9794988027a87e7242
胡宇 <398618174@qq.com>
2024-01-20 10:26:51 +0800

    1

---
+++ shfiles/record-real.sh
@@ -0,0 +1,14 @@
+rosbag record --tcpnodelay \
+/robot/dlio/odom_node/odom \
+/robot/dlio/odom_node/pointcloud/deskewed \
+/particle_map/future_map \
+/particle_map/particle_map \
+/particle_map/inflate_map \
+/drone_0_odom_visualization/path \
+/drone_0_node/topo_list \
+/drone_0_node/optimal_list \
+/drone_0_odom_visualization/robot \
+/dynamic_map_generator/global_cylinders_state \
+/drone_0_odometry \
+/drone_0_node/goal_point \
+-o ~/real_world.bag
